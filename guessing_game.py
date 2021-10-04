f889909ec4b8d4b19e0184ac4db8b1d6f245d88b
Happy <83241650+HappyBravo@users.noreply.github.com>
2021-10-04 23:30:57 +0530

    Add files via upload

--- guessing_game.py
+++ guessing_game.py
@@ -1,140 +1,140 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Sep 25 00:17:55 2021
-
-@author: anish
-"""
-
-
 from itertools import combinations
-from tabulate import tabulate
 import sys
-print('YOU CAN MAKE YOUR OWN NUMBER GUESSING GAME!!')
+import math
+
+#print('YOU CAN MAKE YOUR OWN NUMBER GUESSING GAME!!')
+
+#
+# - - - - - INITIAL INFORMATIONS - - - - - 
+#
+
 print('YOU NEED TO ENTER THE NUMBER UPTO WHICH YOU WANT THE PROGRAM TO MAKE THE GAME FOR YOU')
-q=int(input('ENTER YOUR NUMBER '))
-input("PRESS ENTER TO START THE GAME!!\n")
+
+for i in range(3):
+    # CHECKING IF THE INPUT IS CORRECT
+    try:
+        q = int(input('ENTER YOUR NUMBER : '))
+        break
+    except:
+        print('OOPS,IT SEEMS THAT YOU HAVE NOT RESPONDED CORRECTLY!')
+        if i == 2:
+            print("RETRY!!!")
+            sys.exit()
+
+input("PRESS ENTER TO START THE GAME!!")
 print('---------------------------------------------------------')
-print('GUESSING GAME\n')
-print('STEP1: THINK A NUMBER BETWEEN 1 AND',q)
+print('GUESSING GAME')
+print('STEP 1: THINK A NUMBER BETWEEN 1 AND',q)
 input("Press Enter to continue...")
-print('YOU WILL BE GETTING A NUMBER OF TABLES CONSISTING OF NUMBERS\n')
+print('YOU WILL BE GETTING A NUMBER OF TABLES CONSISTING OF NUMBERS')
 print('STEP 2: ENTER 1 FOR YES AND 0 FOR NO')
+#
+# - - - - - END OF INITIAL INFORMATIONS - - - - -
+#
 
+# DECLARING VARIABLES
+dict_of_numbers = {}
+no_of_tables = 0
+comb = []
 
-a=0
-b=0
-for i in range(1,100000):    
-  r=0
-  for j in range(1,i+1):
-    comb = list(combinations([2**j for j in range(0,i)], j))      #initialising the no. of tables required for the game
-    print(comb)
-    r=r+len(comb)
-    no=j  
-  if r>q:
-    #print(r)        #max number possible according to the given number
-    #print(no)       #no of tables required
-    break
+# GENERATING REQUIRED 1ST ELEMENT NUMBER FOR THE TABLES
+for i in range(1,q):
+    comb = list(combinations([2**_ for _ in range(i)], i))
+    no_of_tables = i 
+    dict_of_numbers["B{0}".format(i-1)] = [2**(i-1)]
+    if 2**(i) > q:
+        break
 
-for i in range(1,no+1):
-  globals()['b%s'%i]= [2**(i-1)]
-  #print(globals()['b%s'%i])            #making new lists with 2**n as the first number i.e E11 element of the every table 
+for _ in range(1, no_of_tables+1):
+    comb1 = list(combinations(comb[0], _))
+    for j in comb1:
+        for k in j:
+            for l in range(no_of_tables):
+                if k in dict_of_numbers["B{0}".format(l)]:
+                    if not (sum(list(j))>q):
+                        dict_of_numbers["B{0}".format(l)].append(sum(list(j)))
+                dict_of_numbers["B{0}".format(l)] = list(set(dict_of_numbers["B{0}".format(l)]))
 
-for i in range(1,no+1):
-  comb = list(combinations([2**j for j in range(0,no)], i))
-  for j in comb:
-    #print (j,sum(j))           #will show the possible combinations and their sum(unique)
-    h= list(j)
-    #print(h,sum(h))
-    for k in h:
-      for l in range(1,no+1):
-        if k in globals()['b%s' % l]:
-          globals()['b%s' % l].append(sum(h))
+#
+# - - - - Printing the Tables - - - -
+#
 
+def nearest_square(no):
+    return (math.ceil(no**0.5))**2
 
-#print(list(set(b1)))           #will show the list(b1) of first table
+def fillup(the_list, upto):
+    temp = ['~']*(upto - len(the_list))
+    the_list += temp
+    return the_list
 
-
-def newlist(x):
-    while len(x)%float((len(x)**0.5))!=0:
-        x.append('~')
-    tablise(x)
+def make_table(_list):
+    n = nearest_square(len(_list))
+    l = int(n**0.5)
+    max_wd = len(str(max(_list)))
     
+    if len(_list)<n:
+        _list = fillup(_list, n)
+   
+    i = 0
+    out_string = ""
+    while i<len(_list):
+        if i%l == 0:
+            # print()
+            # print("+"+"="*(max_wd+3)*l+"+")
+            # print("|", end = "")
+            out_string += "\n+" + "="*(max_wd+3)*l+"+" + "\n|"
+        
+        out_string += " {0:^{1}} |".format(_list[i], max_wd)       
+        i+=1
+    else:
+        # print()
+        # print("+"+"="*(max_wd+3)*l+"+")
+        out_string += "\n" + "+"+"="*(max_wd+3)*l+"+"
     
+    return out_string
 
-def tablise(x):
-  trial_tab1=[]
+#
+# - - - - - End of Printing Tables - - - - -
+#
 
-  for i in range(0,len(x),int(len(x)**0.5)):
-      trial_tab1.append(x[i:i+int((len(x)**0.5))])
- 
-  print(tabulate(trial_tab1,tablefmt="fancy_grid"))
+# make_table([100,2,3,10,2,2000,1,2,1])
+# make_table(dict_of_numbers['B0'])
+# fillup([1,2,3], 3)
 
+#
+# - - - - - MAIN BODY OF PROGRAM - - - -
+#
 
-def check(x):
-  if (x!=1):
-    if (x!=0):
-      print('invalid input')
-      sys.exit()
+ans = 0
+choice = 0
+flag = 0
 
-for i in range(1,no+1):
-  #print(globals()['b%s'%i])
-  no_repeats_list= list(set(globals()['b%s'%i]))
-  reduced_list=[]
-  for j in no_repeats_list:
-    if j<=q:
-      reduced_list.append(j)
-  newlist(reduced_list)
-  
-  globals()['response%s' % i]= int(input('IS YOUR NUMBER HERE?'))
-  check(globals()['response%s' % i])
+for k in range(no_of_tables):
+    print(make_table(dict_of_numbers["B{0}".format(k)]) + "\nIS YOUR NUMBER HERE ?")
+     
+    for _ in range(3):
+        choice = input("(1 for YES, 0 for NO) : ")
+        
+        # CHECKING IF THE INPUT IS CORRECT
+        if choice == "1":
+            ans += 2**k
+            flag = 0
+            break
+        elif choice == "0":
+            flag = 0
+            break
+        else:
+            flag = 1
+            print("PLEASE ENTER 1 FOR YES AND 0 FOR NO")
+    
+    if flag == 1: # FOR WRONG INPUT
+        print('OOPS,IT SEEMS THAT YOU HAVE NOT RESPONDED CORRECTLY!')
+        print("RETRY!!!")
+        sys.exit()
 
-  
-table_dictionary={}
-for i in range(0,no+1):  
-   table_dictionary["table%s" %(i+1)] = 2**(i)
-print(table_dictionary)
-
-ans=0
-
-for j in range(1,no+1):
-  if globals()['response%s'%j]==1:
-    ans=ans+ table_dictionary['table%s'%j]
-if ans>q:
-  print('OOPS,IT SEEMS THAT YOU HAVE NOT RESPONDED CORRECTLY!')
+# PRINTING RESULTS
+if ans > q or  (q == 0 and ans == 0):
+    print('\nOOPS,IT SEEMS THAT YOU HAVE NOT RESPONDED CORRECTLY!')
 else:
-  print('\nYOUR NUMBER IS',ans)
+    print('\nYOUR NUMBER IS',ans)
 print('---------------------------------------------------------') 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
